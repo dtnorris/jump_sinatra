4387e1c3f90cbba20208e6b92dac027793570c5e
David Tengdin <dreamingfurther@gmail.com>
2016-11-13 12:14:37 -0500

    setup Sintra

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # jump_sinatra
+
+To run the app with rerun do this:
+
+```
+1. gem install rerun
+2. rerun 'ruby server.rb'
+```
---
+++ server.rb
@@ -0,0 +1,8 @@
+require "sinatra"
+
+set :bind, '0.0.0.0'  # bind to all interfaces
+set :public_folder, File.join(File.dirname(__FILE__), "public")
+
+get "/" do
+  erb :home
+end
